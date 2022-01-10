b91768659d187b39dc4773f21b56bc31cda66844
vinayaachar <vinayaachar@gmail.com>
2022-01-09 23:02:34 -0800

    Add fetch API routes to json server

--- src/context/FeedbackContext.jsx
+++ src/context/FeedbackContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 import {v4 as uuidv4} from 'uuid'
 
 
@@ -6,33 +6,25 @@ const FeedbackContext = createContext()
 
 export const FeedbackProvider = ({children}) => {
 
-  const [feedback, setFeedback] = useState([
-    {
-      id: 1,
-      text: 'This is feedback item 1',
-      rating: 10
-    },
-    {
-      id: 2,
-      text: 'This is feedback item 2',
-      rating: 8
-    },
-    {
-      id: 3,
-      text: 'This is feedback item 3',
-      rating: 9
-    },
-    {
-      id: 4,
-      text: 'This is feedback item 4',
-      rating: 7
-    },
-  ])
+  const [feedback, setFeedback] = useState([])
+
+  const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(() => {
+    fetchData()
+  }, [])
 
   const [feedbackEdit, setFeedbackEdit] = useState({
     item: {},
     edit: false
   })
+
+  const fetchData = async () => {
+    const response = await fetch(`http://localhost:5000/feedback`)
+    const data = await response.json()
+    setFeedback(data)
+    setIsLoading(false)
+  }
 
   const deleteFeedback = (id) => {
     if (window.confirm('Are you sure you want to delete')) {
@@ -60,6 +52,7 @@ export const FeedbackProvider = ({children}) => {
   return <FeedbackContext.Provider value={{
     feedback,
     feedbackEdit,
+    isLoading,
     deleteFeedback,
     feedbackAdd,
     editFeedback,
